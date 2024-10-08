16cee7844aba67f00ce36bce5e3d2462ce6ba663
Jabrane Ben Hadj Messaoud <jabrane.benhadjmessaoud@gmail.com>
2024-10-08 10:53:23 +0100

    DojoPizzeria practice and moving code review outside

---
+++ Week_2/Day_2/Practice/DojoPizzeria.js
@@ -0,0 +1,62 @@
+// Creating a function called pizzaOven in JS
+
+function pizzaOven(){
+
+
+}
+
+// *****************************************
+
+
+// creating first pizza
+
+
+var pizza1 = {
+    crustType:    "deep dish",
+    sauceType:  "traditional",
+    cheeses:   "Mozzarella",
+    toppings: ["pepperoni", "sausage"]
+};
+    
+console.log(pizza1);
+
+
+// ************************************
+
+// creating second pizza 
+
+var pizza2 = {
+    crustType:    "hand tossed",
+    sauceType:  "marinara",
+    cheeses:   ["Mozzarella","feta"],
+    toppings: ["mushrooms", "olives","onions"]
+};
+    
+console.log(pizza2);
+
+// creating 2 other pizza with topping we like 
+
+var pizza3 = {
+    crustType:    "fluffy",
+    sauceType:  "white sauce",
+    cheeses:   ["Mozzarella","chedar","camembert","gorgonzolla"],
+    toppings: ["tuna","onions"]
+};
+    
+console.log(pizza4);
+var pizza2 = {
+    crustType:    "crispy",
+    sauceType:  "tomato sauce",
+    cheeses:   "Mozzarella",
+    toppings: ["chicken", "onion rings","tomato rings","mushrooms"]
+};
+    
+console.log(pizza4);
+
+
+// creating a function to get a random pizza 
+
+function randomPizza(){
+
+
+}
